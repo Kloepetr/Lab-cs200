acd484a9241c88dac4637754f96e8a8f54e17d07
Kloe <115853526+Kloepetr@users.noreply.github.com>
2025-04-05 13:02:44 -0400

    Add files via upload

---
+++ utility/lab6.py
@@ -0,0 +1,15 @@
+'''
+File Name: lab6.py
+Purpose:
+This file calls out functions from student_data
+First Create Date: October 3rd, 2024
+Last Update Date: October 3rd, 2024
+Author: Xiaoyuan Wang
+Version: 1.1
+'''
+
+
+#import my executable file.
+from student_data import student_data ;
+#call out function.
+student_data();
